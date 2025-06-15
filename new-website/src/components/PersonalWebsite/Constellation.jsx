23df91c63addeac5908ef294a902567c4de79c87
Arav Dhoot <aravdhoot@gmail.com>
2025-06-15 12:45:53 -0700

    clean

--- new-website/src/components/PersonalWebsite/Constellation.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function Constellation({ isDark }) {
-  // same implementation as before, returning stars & lines
-  return <div className="absolute inset-0 pointer-events-none">Constellation placeholder</div>
-}
